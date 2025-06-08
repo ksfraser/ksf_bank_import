2e7f6ad0993f771d49626761b3df95082af96827
Kevin Fraser <kevin@ksfraser.ca>
2025-06-07 20:31:41 -0600

    deleted some dupes

--- src/Ksfraser/FaBankImport/src/Ksfraser/Application/interfaces/CommandBusInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ksfraser\Application\Interfaces;
-
-interface CommandBusInterface
-{
-    public function dispatch($command);
-    public function register(string $commandClass, $handler): void;
-}
