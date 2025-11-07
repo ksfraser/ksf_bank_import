00fd1a69e5e6ba29d2fdeaac36c680dc58a060b0
Kevin Fraser <kevin@ksfraser.ca>
2025-11-06 22:27:08 -0700

    feat: Add query parameter support to HtmlLink
    - Implemented addParam(key, value) method for building query strings
    - Added protected updateHref() method using http_build_query()
    - Added \ and \ properties for state management
    - Added missing constructor to set tag='a'
    - Enhanced PHPDoc for all methods
    - All 14 tests passing (was 12/14)

--- src/Ksfraser/HTML/Elements/HtmlLink.php
+++ src/Ksfraser/HTML/Elements/HtmlLink.php
@@ -13,12 +13,34 @@ use Ksfraser\HTML\HtmlAttribute;
 */
 class HtmlLink extends HtmlElement
 {
-	//can have styles, title
-	function __construct( HtmlElementInterface $data )
+	/**
+	 * @var array Query parameters to append to URL
+	 */
+	protected $params = [];
+	
+	/**
+	 * @var string Base URL without query parameters
+	 */
+	protected $baseUrl = '';
+	
+	/**
+	 * Constructor - creates an <a> link element
+	 * 
+	 * @param HtmlElementInterface $data Initial nested content
+	 */
+	function __construct(HtmlElementInterface $data)
 	{
-		parent::__construct( $data );
+		parent::__construct($data);
 		$this->tag = "a";
 	}
+	
+	/**
+	 * Add href attribute to the link, optionally with link text
+	 * 
+	 * @param string $url The URL for the link
+	 * @param string|HtmlElementInterface $text Optional link text or element
+	 * @return void
+	 */
 	function addHref( $url, $text = "" )
 	{
 		// If text is provided (backwards compatibility)
@@ -33,9 +55,50 @@ class HtmlLink extends HtmlElement
 		}
 		// If no text provided, leave data as already set by constructor
 		
-		// Set the href attribute
+		// Store base URL for later param appending
+		$this->baseUrl = $url;
+		
+		// Set the href attribute (will be updated if params are added)
+		$this->updateHref();
+	}
+	
+	/**
+	 * Add a query parameter to the URL
+	 * 
+	 * @param string $key Parameter name
+	 * @param string $value Parameter value
+	 * @return self For method chaining
+	 */
+	function addParam( string $key, string $value ): self
+	{
+		$this->params[$key] = $value;
+		$this->updateHref();
+		return $this;
+	}
+	
+	/**
+	 * Update the href attribute with base URL + query params
+	 * 
+	 * @return void
+	 */
+	protected function updateHref(): void
+	{
+		$url = $this->baseUrl;
+		
+		if( !empty($this->params) )
+		{
+			$url .= '?' . http_build_query($this->params);
+		}
+		
 		$this->addAttribute( new HtmlAttribute( "href", $url ) );
 	}
+	/**
+	 * Set the target attribute for the link
+	 * 
+	 * @param string $target Target type (_self, _blank, _parent, _top)
+	 * @return void
+	 * @throws \Exception If target type is not recognized
+	 */
 	function setTarget( $target )
 	{
 		//Target can be _self, _blank, _parent, _top
