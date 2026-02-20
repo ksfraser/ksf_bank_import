1e7da1b8b677543d109b78ec06e81f638b2a4fc5
Kevin Fraser <kevin@ksfraser.ca>
2026-02-19 20:20:39 -0700

    fix path to FA includes

--- import_statements.php
+++ import_statements.php
@@ -21,8 +21,8 @@ $page_security = 'SA_BANKACCOUNT';
 if (!@include(__DIR__ . '/includes/session.inc')) {
 	include_once(__DIR__ . '/includes/fa_stubs.php');
 }
-@include_once(__DIR__ . '/includes/date_functions.inc');
-@include_once(__DIR__ . '/includes/ui.inc');
+@include_once( __DIR__ . '/../../includes/date_functions.inc');
+@include_once(__DIR__ . '/../../includes/ui.inc');
 
 
 // Use local includes for banking and parsers
